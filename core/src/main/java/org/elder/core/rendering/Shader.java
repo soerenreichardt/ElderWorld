cb2bfc1e6101eb16749b28855508416ad2d3d680
Sören Reichardt <soren.reichardt@neotechnology.com>
2022-03-25 17:39:18 +0100

    First draft shader abstraction

---
+++ core/src/main/java/org/elder/core/rendering/Shader.java
@@ -0,0 +1,164 @@
+package org.elder.core.rendering;
+
+import org.lwjgl.BufferUtils;
+import org.lwjgl.opengl.GL;
+import org.lwjgl.opengl.GLCapabilities;
+import org.lwjgl.system.MemoryStack;
+
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.UncheckedIOException;
+import java.nio.ByteBuffer;
+import java.nio.channels.Channels;
+import java.nio.channels.ReadableByteChannel;
+import java.nio.channels.SeekableByteChannel;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+import static org.lwjgl.BufferUtils.createByteBuffer;
+import static org.lwjgl.opengl.GL20.*;
+import static org.lwjgl.system.MemoryStack.stackPush;
+import static org.lwjgl.system.MemoryUtil.memSlice;
+
+public class Shader {
+
+    private static final int UNINITIALIZED = -1;
+
+    private final Path vertexShaderPath;
+    private final Path fragmentShaderPath;
+    private final int version;
+    private int program;
+
+    public Shader(Path vertexShaderPath, Path fragmentShaderPath) {
+        this.vertexShaderPath = vertexShaderPath;
+        this.fragmentShaderPath = fragmentShaderPath;
+
+        GLCapabilities caps = GL.getCapabilities();
+        int version;
+        if (caps.OpenGL33) {
+            version = 330;
+        } else if (caps.OpenGL21) {
+            version = 120;
+        } else {
+            version = 110;
+        }
+        this.version = version;
+        this.program = UNINITIALIZED;
+    }
+
+    public void compile() {
+        var vertexShader = glCreateShader(GL_VERTEX_SHADER);
+        var fragmentShader = glCreateShader(GL_FRAGMENT_SHADER);
+
+        compileShader(vertexShaderPath, vertexShader);
+        compileShader(fragmentShaderPath, fragmentShader);
+
+        var program = glCreateProgram();
+        glAttachShader(program, vertexShader);
+        glAttachShader(program, fragmentShader);
+        glLinkProgram(program);
+
+        printProgramInfoLog(program);
+
+        if (glGetProgrami(program, GL_LINK_STATUS) != GL_TRUE) {
+            throw new IllegalStateException("Failed to link program.");
+        }
+
+        this.program = program;
+    }
+
+    public void use() {
+        if (program == UNINITIALIZED) {
+            throw new IllegalStateException("Expected shader to be compiled and linked to a program");
+        }
+        glUseProgram(program);
+    }
+
+    private ByteBuffer readShaderFile(Path shaderPath) {
+        try {
+            return ioResourceToByteBuffer(shaderPath, 4096);
+        } catch (IOException e) {
+            throw new UncheckedIOException(e);
+        }
+    }
+
+    private void compileShader(Path shaderPath, int shader) {
+        var byteBuffer = readShaderFile(shaderPath);
+        compileShader(version, shader, byteBuffer);
+    }
+
+    private void compileShader(int version, int shader, ByteBuffer code) {
+        try (MemoryStack stack = stackPush()) {
+            ByteBuffer header = stack.ASCII("#version " + version + "\n#line 0\n", false);
+
+            glShaderSource(
+                    shader,
+                    stack.pointers(header, code),
+                    stack.ints(header.remaining(), code.remaining())
+            );
+
+            glCompileShader(shader);
+            printShaderInfoLog(shader);
+
+            if (glGetShaderi(shader, GL_COMPILE_STATUS) != GL_TRUE) {
+                throw new IllegalStateException("Failed to compile shader.");
+            }
+        }
+    }
+
+
+    public static ByteBuffer ioResourceToByteBuffer(Path path, int bufferSize) throws IOException {
+        ByteBuffer buffer;
+
+        if (Files.isReadable(path)) {
+            try (SeekableByteChannel fc = Files.newByteChannel(path)) {
+                buffer = createByteBuffer((int) fc.size() + 1);
+                while (fc.read(buffer) != -1) {
+                }
+            }
+        } else {
+            try (
+                    InputStream source = Shader.class.getClassLoader().getResourceAsStream(path.toString());
+                    ReadableByteChannel rbc = Channels.newChannel(source)
+            ) {
+                buffer = createByteBuffer(bufferSize);
+
+                while (true) {
+                    int bytes = rbc.read(buffer);
+                    if (bytes == -1) {
+                        break;
+                    }
+                    if (buffer.remaining() == 0) {
+                        buffer = resizeBuffer(buffer, buffer.capacity() * 3 / 2); // 50%
+                    }
+                }
+            }
+        }
+
+        buffer.flip();
+        return memSlice(buffer);
+    }
+
+    private static ByteBuffer resizeBuffer(ByteBuffer buffer, int newCapacity) {
+        ByteBuffer newBuffer = BufferUtils.createByteBuffer(newCapacity);
+        buffer.flip();
+        newBuffer.put(buffer);
+        return newBuffer;
+    }
+
+    private static void printShaderInfoLog(int obj) {
+        int infoLogLength = glGetShaderi(obj, GL_INFO_LOG_LENGTH);
+        if (infoLogLength > 0) {
+            glGetShaderInfoLog(obj);
+            System.out.format("%s\n", glGetShaderInfoLog(obj));
+        }
+    }
+
+    private static void printProgramInfoLog(int obj) {
+        int infoLogLength = glGetProgrami(obj, GL_INFO_LOG_LENGTH);
+        if (infoLogLength > 0) {
+            glGetProgramInfoLog(obj);
+            System.out.format("%s\n", glGetProgramInfoLog(obj));
+        }
+    }
+}
