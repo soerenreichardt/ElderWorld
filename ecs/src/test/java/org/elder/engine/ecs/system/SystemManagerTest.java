e36312557b610ef2537915f483b72a893ded0b58
Sören Reichardt <soren.reichardt@neotechnology.com>
2022-11-10 21:45:54 +0100

    Test SceneRepository side effect in SystemManager

--- ecs/src/test/java/org/elder/engine/ecs/system/SystemManagerTest.java
+++ ecs/src/test/java/org/elder/engine/ecs/system/SystemManagerTest.java
@@ -1,5 +1,7 @@
 package org.elder.engine.ecs.system;
 
+import org.elder.engine.ecs.SceneRepository;
+import org.elder.engine.ecs.api.BasicScene;
 import org.elder.engine.ecs.system.test.TestSystem;
 import org.junit.jupiter.api.Test;
 
@@ -27,6 +29,18 @@ class SystemManagerTest {
 
         assertThat(systemManager.loadedSystems()).hasSize(1);
         assertThat(systemManager.loadedSystems()).hasExactlyElementsOfTypes(TestSystem.class);
+    }
+
+    @Test
+    void shouldUpdateSceneRepositoryOnSceneChanged() {
+        var systemManager = SystemManager.builder().fromList().build();
+        systemManager.loadSystems();
+        assertThat(SceneRepository.getScene()).isNull();
+
+        var scene = new BasicScene("Test");
+        systemManager.onSceneChanged(scene);
+
+        assertThat(SceneRepository.getScene()).isEqualTo(scene);
     }
 
 }
