4a3a5b91ddc460ab7a57574e3e32f13178403ec3
Sören Reichardt <soren.reichardt@neotechnology.com>
2022-12-30 14:08:51 +0100

    Allow registration of additional systems in server mode

--- engine/src/main/java/org/elder/engine/server/ServerGameEngine.java
+++ engine/src/main/java/org/elder/engine/server/ServerGameEngine.java
@@ -4,29 +4,42 @@ import org.elder.engine.api.GameEngine;
 import org.elder.engine.api.GameExecutable;
 import org.elder.engine.ecs.api.BasicScene;
 import org.elder.engine.ecs.api.Resource;
+import org.elder.engine.ecs.api.UpdatableSystem;
 import org.elder.engine.ecs.system.SystemManager;
 import org.elder.engine.ecs.system.SystemManagerBuilder;
 import org.elder.engine.physics.PositioningSystem;
 import org.elder.engine.script.ScriptSystem;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 
 public class ServerGameEngine extends GameEngine<BasicScene> {
 
     private final AtomicBoolean exitFlag;
+    private final List<Class<? extends UpdatableSystem>> additionalSystems;
 
-    protected ServerGameEngine(GameExecutable<BasicScene> gameExecutable, AtomicBoolean exitFlag, Resource[] resources) {
+    protected ServerGameEngine(
+            GameExecutable<BasicScene> gameExecutable,
+            AtomicBoolean exitFlag,
+            List<Class<? extends UpdatableSystem>> additionalSystems,
+            Resource[] resources
+    ) {
         super(gameExecutable, resources);
         this.exitFlag = exitFlag;
+        this.additionalSystems = additionalSystems;
     }
 
     @Override
     protected SystemManager initializeSystemManager(SystemManagerBuilder systemManagerBuilder, Resource[] resources) {
-        return systemManagerBuilder.fromList()
+        var systemManagerBuilderWithDefaultSystems = systemManagerBuilder.fromList()
                 .withResources(resources)
                 .withSystemClass(PositioningSystem.class)
                 .withSystemClass(ScriptSystem.class)
-                .skipMissingDependencies()
+                .skipMissingDependencies();
+
+        additionalSystems.forEach(systemManagerBuilderWithDefaultSystems::withSystemClass);
+
+        return systemManagerBuilderWithDefaultSystems
                 .build();
     }
 
--- engine/src/main/java/org/elder/engine/server/ServerGameEngineRunner.java
+++ engine/src/main/java/org/elder/engine/server/ServerGameEngineRunner.java
@@ -6,24 +6,28 @@ import org.elder.engine.ecs.ComponentRegistry;
 import org.elder.engine.ecs.Transform;
 import org.elder.engine.ecs.api.BasicScene;
 import org.elder.engine.ecs.api.Resource;
+import org.elder.engine.ecs.api.UpdatableSystem;
 import org.elder.engine.physics.Velocity;
 import org.elder.engine.script.Script;
 
+import java.util.List;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.concurrent.locks.LockSupport;
 
 public class ServerGameEngineRunner implements GameEngineRunner<BasicScene> {
 
     private final AtomicBoolean exitFlag;
+    private final List<Class<? extends UpdatableSystem>> systems;
 
-    public ServerGameEngineRunner() {
+    public ServerGameEngineRunner(List<Class<? extends UpdatableSystem>> systems) {
+        this.systems = systems;
         this.exitFlag = new AtomicBoolean(false);
         registerComponents();
     }
 
     @Override
     public void start(GameExecutable<BasicScene> gameExecutable) throws InterruptedException {
-        var gameEngine = new ServerGameEngine(gameExecutable, exitFlag, new Resource[0]);
+        var gameEngine = new ServerGameEngine(gameExecutable, exitFlag, systems, new Resource[0]);
         gameEngine.start();
         while (!exitFlag.get()) {
             LockSupport.park(100_000_000);
